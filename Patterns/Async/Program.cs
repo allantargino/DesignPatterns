644cb919cae2c34c8e8e07d2b49fbca458691bbc
Allan Targino <altargin@microsoft.com>
2017-08-31 22:29:50 -0300

    real async

--- Patterns/Async/Program.cs
+++ Patterns/Async/Program.cs
@@ -5,18 +5,13 @@ using System.Threading.Tasks;
 
 namespace State
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            MainAsync(args).Wait();
-        }
-
-        static async Task MainAsync(string[] args)
+        public static async Task Main(string[] args)
         {
             // obtem dados iniciais
             var inputs = new CartesianInput(new CartesianPoint(50, 25), new CartesianPoint(100, 100));
-            
+
             // usando uma regressao linear simples
             var trainer = new LinearRegressionTrainer(iterations: 5);
 
@@ -26,7 +21,7 @@ namespace State
             // processa resultado
             var list = (List<float>)model.LinearParameters;
             Console.WriteLine($"Result: y = {list[0]} + {list[1]} x");
-            
+
             Console.ReadLine();
         }
     }
